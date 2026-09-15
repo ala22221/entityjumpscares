66ff1cdb25ffc936c4f6c14db18e454723bda8be
ala22221 <ffd000ac@gmail.com>
2026-09-15 18:50:52 +0500

    fixed the size shit not working

--- reboundscare.lua
+++ reboundscare.lua
@@ -4,17 +4,14 @@ local TweenService = game:GetService("TweenService")
 local player = Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 
-
 local REBOUND_IMAGE = "rbxassetid://94841181437395"
 local REBOUND_SOUND = "rbxassetid://88369282881781"
-
 
 local gui = Instance.new("ScreenGui")
 gui.Name = "ReboundJumpscare"
 gui.IgnoreGuiInset = true
 gui.ResetOnSpawn = false
 gui.Parent = playerGui
-
 
 local bg = Instance.new("Frame")
 bg.Name = "BGC"
@@ -25,11 +22,10 @@ bg.BackgroundTransparency = 1
 bg.BorderSizePixel = 0
 bg.Parent = gui
 
-
 local rebound = Instance.new("ImageLabel")
 rebound.Name = "Rebound"
-rebound.Size = UDim2.fromScale(0.5, 0.5)
-rebound.Position = UDim2.fromScale(0, 0)
+rebound.Size = UDim2.fromScale(0.25, 0.25)
+rebound.Position = UDim2.fromScale(0.375, 0.375)
 rebound.BackgroundTransparency = 1
 rebound.Image = REBOUND_IMAGE
 rebound.ScaleType = Enum.ScaleType.Stretch
@@ -43,13 +39,11 @@ scream.Volume = 1
 scream.PlaybackSpeed = 3
 scream.Parent = gui
 
-
 local pitch = Instance.new("PitchShiftSoundEffect")
 pitch.Name = "Pitch"
 pitch.Octave = 0.5
 pitch.Priority = 0
 pitch.Parent = scream
-
 
 local equalizer = Instance.new("EqualizerSoundEffect")
 equalizer.Name = "Equalizer"
@@ -59,24 +53,22 @@ equalizer.MidGain = 10
 equalizer.Priority = 0
 equalizer.Parent = scream
 
-
 local distortion = Instance.new("DistortionSoundEffect")
 distortion.Name = "Distortion"
 distortion.Level = 0.75
 distortion.Priority = 0
 distortion.Parent = scream
 
--- Jumpscare
 local function reboundJumpscare()
 
-	-- Reset
 	bg.BackgroundTransparency = 1
 	bg.BackgroundColor3 = Color3.fromRGB(2, 25, 99)
 
 	rebound.Visible = true
-	rebound.Size = UDim2.fromScale(1, 1)
-	rebound.Position = UDim2.fromScale(0, 0)
 
+
+	rebound.Size = UDim2.fromScale(0.25, 0.25)
+	rebound.Position = UDim2.fromScale(0.375, 0.375)
 
 	TweenService:Create(
 		bg,
@@ -87,11 +79,9 @@ local function reboundJumpscare()
 		}
 	):Play()
 
-
 	scream:Play()
 
 	task.wait(0.2)
-
 
 	TweenService:Create(
 		rebound,
@@ -101,7 +91,6 @@ local function reboundJumpscare()
 			Position = UDim2.new(-0.499, 0, -0.496, 0)
 		}
 	):Play()
-
 
 	TweenService:Create(
 		bg,
@@ -119,14 +108,11 @@ local function reboundJumpscare()
 
 	task.wait(0.4)
 
-
 	rebound.Visible = false
 	scream:Stop()
 
-	-- Reset
 	bg.BackgroundTransparency = 1
 	bg.BackgroundColor3 = Color3.fromRGB(2, 25, 99)
 end
-
 
 reboundJumpscare()
